e90e8cd2a889ecbe997f7adb99374d8cec759c8c
benjmor <44407400+benjmor@users.noreply.github.com>
2021-08-08 23:58:11 -0500

    remove crap

--- Facebook LightsOut/master.js
+++
@@ -1,6 +0,0 @@
-const turnoff = require('./turnofflights');
-const turnon = require('./turnonlights');
-//setTimeout(function(){turnoff.off()}, 5000);
-setTimeout(function(){turnon.on()}, 5000);
-//setTimeout(function(){turnoff.off()}, 5000);
-//setTimeout(function(){turnon.on()}, 5000);
